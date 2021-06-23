ba06d98abca72a1cf4f777f8f52c9c92acc6f5d5
Mohammed Zakaria Alalaya <mohammed.alalaya@gmail.com>
2021-06-23 02:03:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+class Cat {
+  constructor(name, gender){
+    this.name = name;
+    this.gender = gender;
+  } 
+  
+  get speack(){
+    return this.name + " says meow!"
+  }
+}
+
+const cat = new Cat("Sasha", "female");
+cat.speack();
